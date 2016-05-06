705c52059f655d6a0b8fddee60e9745b287e7db0
Mukunda Modell <mmodell@wikimedia.org>
2016-05-05 20:07:02 -0500

    Custom field to output a command which will apply the revision's current diff without using arcanist
    Summary:
    `DifferentialApplyPatchWithOnlyGitField` - a custom field to Differential revisions which displays the commands needed to apply the patch to a local git repo using only `curl` and `git apply`
    
    Reviewers: demon, greg, mmodell, Luke081515
    
    Reviewed By: Luke081515
    
    Differential Revision: https://phabricator.wikimedia.org/D225

--- __phutil_library_map__.php
+++ __phutil_library_map__.php
@@ -10,6 +10,7 @@ phutil_register_library_map(array(
   '__library_version__' => 2,
   'class' => array(
     'CustomLoginHandler' => 'src/CustomLoginHandler.php',
+    'DifferentialApplyPatchWithOnlyGitField' => 'src/DifferentialApplyPatchWithOnlyGitField.php',
     'GerritApplication' => 'src/GerritApplication.php',
     'GerritChangeIdField' => 'src/GerritChangeIdField.php',
     'GerritProjectController' => 'src/GerritProjectController.php',
@@ -22,6 +23,7 @@ phutil_register_library_map(array(
   'function' => array(),
   'xmap' => array(
     'CustomLoginHandler' => 'PhabricatorAuthLoginHandler',
+    'DifferentialApplyPatchWithOnlyGitField' => 'DifferentialCustomField',
     'GerritApplication' => 'PhabricatorApplication',
     'GerritChangeIdField' => 'PhabricatorCommitCustomField',
     'GerritProjectController' => 'PhabricatorController',
