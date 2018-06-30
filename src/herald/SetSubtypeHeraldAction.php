6ad714f4e46aeafbc3599852ca85eefb70f9ef13
Mukunda Modell <mmodell@wikimedia.org>
2018-06-30 06:39:21 -0500

    Custom herald action that sets task subtype
    Summary:
    Very similar to "change priority to" action but changes the
    subtype of matching tasks.
    
    Test Plan: tested locally
    
    Reviewers: Paladox
    
    Subscribers: greg
    
    Differential Revision: https://phabricator.wikimedia.org/D1074

---
+++ src/herald/SetSubtypeHeraldAction.php
@@ -0,0 +1,76 @@
+<?php
+// @TODO: remove this file once the herald rule that uses it is removed
+// from production.
+
+class SetSubtypeHeraldAction extends HeraldAction {
+  const ACTIONCONST = 'maniphest.subtype';
+  const DO_SUBTYPE = 'do.subtype';
+
+  public function getActionGroupKey() {
+    return HeraldApplicationActionGroup::ACTIONGROUPKEY;
+  }
+
+  public function supportsObject($object) {
+    return $object instanceof ManiphestTask;
+  }
+
+  public function supportsRuleType($rule_type) {
+    return $rule_type == HeraldRuleTypeConfig::RULE_TYPE_GLOBAL;
+  }
+
+  public function getActionKey() {
+    return self::ACTIONCONST;
+  }
+
+  public function getHeraldActionName() {
+    return pht('Change type to');
+  }
+
+  public function renderActionDescription($value) {
+    $type = head($value);
+    return pht("Change type to \"%s\"", $type);
+  }
+
+  public function getHeraldActionStandardType() {
+    return self::STANDARD_PHID_LIST;
+  }
+
+  protected function getDatasource() {
+    return id(new ManiphestTaskSubtypeDatasource())
+      ->setLimit(1);;
+  }
+
+  protected function getDatasourceValueMap() {
+    return id(new ManiphestTask())->newEditEngineSubtypeMap();
+  }
+
+  public function applyEffect($object,  HeraldEffect $effect) {
+    $new_subtype = head($effect->getTarget());
+
+    $adapter = $this->getAdapter();
+    $adapter->queueTransaction(id(new ManiphestTransaction())
+      ->setTransactionType(PhabricatorTransactions::TYPE_SUBTYPE)
+      ->setNewValue($new_subtype));
+
+    $this->logEffect(self::DO_SUBTYPE, $new_subtype);
+  }
+
+  protected function getActionEffectMap() {
+    return array(
+      self::DO_SUBTYPE => array(
+        'icon' => 'fa-pencil',
+        'color' => 'green',
+        'name' => pht('Changed Type'),
+      ),
+    );
+  }
+
+  protected function renderActionEffectDescription($type, $data) {
+    switch ($type) {
+      case self::DO_SUBTYPE:
+        return pht('Change type to "%s."', $data);
+    }
+  }
+
+
+}
